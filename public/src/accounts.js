04f68b4d30b1100d55b3f15db777e65c9c54d666
Steven Gebhardt <stevengebhardt@stevens-mbp.lan>
2023-09-19 22:30:54 -0400

    solved the getPossessedByAccount function

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -21,7 +21,20 @@ function getTotalNumberOfBorrows(account = {}, books = []) {
   return totalBorrows;
 }
 
-function getBooksPossessedByAccount(account, books, authors) {}
+function getBooksPossessedByAccount(account = {}, books = [], authors = []) {
+  //loop through books array to find books still borrowed whose author matches authors in the author array
+  let foundBooks = books.filter(
+    (book) =>
+      book.borrows[0].returned === false && book.borrows[0].id === account.id
+  );
+
+  //loop through the found books array and find the matching author for each book and add a new author key whose value is the matching author's object
+  foundBooks.forEach((book) => {
+    const foundAuthor = authors.find((author) => book.authorId === author.id);
+    book.author = foundAuthor;
+  });
+  return foundBooks;
+}
 
 module.exports = {
   findAccountById,
